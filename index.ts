d956b1c6342be88bd582bfe17f164f96d3c92591
Керимкулов Эрик <erik.kerimkulov@gazprombank.ru>
2023-07-18 17:42:56 +0300

    add frontend

--- index.ts
+++ index.ts
@@ -8,6 +8,12 @@ import {
   handleDisconnect,
 } from "./src/handlers";
 import WebSocket from "ws";
+import { httpServer } from "./src/http_server/index";
+
+const HTTP_PORT = 8181;
+
+console.log(`Start static http server on the ${HTTP_PORT} port!`);
+httpServer.listen(HTTP_PORT);
 
 const server = new WebSocket.Server({ port: 3000 });
 
